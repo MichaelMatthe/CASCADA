94d288d53079bea84a74198661fb0df653689d89
Michael Matthé <michael.matthe@ipvs.uni-stuttgart.de>
2024-10-11 17:33:19 +0200

    implement simulator interface, sensor and effector interfaces

--- use_cases/adaptation_logic.py
+++ use_cases/adaptation_logic.py
@@ -1,39 +1,75 @@
 import pandas
 from models.cmab import CMAB
+from models.feature_model import NumericalFM
 
 
 class AdaptationLogic:
 
-    def __init__(self, simulation_client, cmab: CMAB):
-        self.simulation_client = simulation_client
+    def __init__(
+        self,
+        simulation_interface: "SimulatorInterface",
+        cmab: CMAB,
+        feature_model: NumericalFM,
+    ) -> None:
+        self.simulation_interface = simulation_interface
         self.cmab = cmab
 
-    def run(self, num_runs=1):
+        self.feature_model = feature_model
+        self.valid_configurations = self.feature_model.generate_numerical_truth_table()
+        self.context_features = self.feature_model.context_feature_names
+        self.system_features = self.feature_model.system_feature_names
 
-        # 1 get context
-        current_context = self.monitor_context()
+    def run(self, num_runs=1, adaptation_loop_interval=5):
 
-        # 2 select arm based on context from cmab and reconfigure
-        selected_configuration = self.cmab.select_arm(current_context)
-        self.excute(selected_configuration)
+        self.simulation_interface.connect_to_simulator()
 
-        # 3 monitoring of performance (delayed?)
-        reward = self.monitor_performance()
+        for run in range(num_runs):
+            # 1 Monitor
+            current_configuration, reward = self.monitor()
+            print(f"--- Monitoring: \n{current_configuration}, \nreward: {reward}")
 
-        # 4 update arm
-        current_config = self.get_current_system_context_configuration()
-        self.cmab.update_arm(current_config, reward)
+            if self.delayed_feedback_available():
+                # Delayed feedback / reward if latency involved
+                self.cmab.update_arm(current_configuration, reward)
 
+                # 2 Analysis and Plan
+                selected_configuration = self.cmab.select_arm(current_configuration)
+
+                # 3 Execute
+                self.execute(selected_configuration)
+
+            # Function for timing the AL to the given interval
+
+        self.simulation_interface.disconnect_from_simulator()
+
+    def monitor(self) -> tuple[pandas.Series, float]:
         pass
 
-    def monitor_context(self) -> pandas.Series:
+    def delayed_feedback_available(self) -> bool:
         pass
 
-    def excute(self, configuration: pandas.Series) -> None:
+    def analysis_and_plan(self) -> pandas.Series:
         pass
 
-    def monitor_performance(self) -> float:
+    def execute(self, system_configuration: pandas.Series) -> None:
         pass
 
-    def get_current_system_context_configuration(self) -> pandas.Series:
+
+class SimulatorInterface:
+
+    def __init__(self, feature_model: NumericalFM) -> None:
+        self.feature_model = feature_model
+
+    def sensor_interface(self) -> tuple[pandas.Series, float]:
+        # Translate simulator values to pandas.Series Feature Model configuration
+        pass
+
+    def effector_interface(self, configuration: pandas.Series) -> None:
+        # Translate pandas.Series configuration of Feature Model to simulator values
+        pass
+
+    def connect_to_simulator(self) -> None:
+        pass
+
+    def disconnect_from_simulator(self) -> None:
         pass
